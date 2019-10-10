c2ea3786e47779d98423fb4267287c393e6cbd2c
inging <55909533+Autchadaporn@users.noreply.github.com>
2019-10-10 16:54:26 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cat x = new cat();
+            x.setnameCat(tb1.Text);
+            tb2.Text = x.getnameCat();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+class cat {
+    private string nameCat;
+    public void setnameCat(string name) {
+        this.nameCat = name;
+    }
+    public string getnameCat() {
+        return this.nameCat;
+    }
+}
